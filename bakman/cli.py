5f8b165c247f035b5aea3c174eab774277a021ac
Wen Li <spacelis@gmail.com>
2016-11-05 16:18:49 +0000

    Rearrange layout of modules

--- bakman/cli.py
+++ bakman/cli.py
@@ -1,15 +1,86 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+File: cli.py
+Author: Wen Li
+Email: spacelis@gmail.com
+Github: http://github.com/spacelis
+Description: Commandline interface for Bakman
+"""
 
+import sys
 import click
+from bakman.labeller import RegexLabeller, StatLabeller, BinderLabeller
+from bakman.marker import Marker
+
+
+def load_rules(rulefile, use_json=False):
+    """ Load rule file content into a dict.
+
+    :rulefile: TODO
+    :returns: TODO
+
+    """
+    if use_json:
+        import json
+        with open(rulefile) as fin:
+            return json.load(fin)
+    else:
+        import yaml
+        with open(rulefile) as fin:
+            return yaml.load(fin)
+
+
+def print_actions(fout, marks):
+    """ Print the action script to the fout
+
+    :fout: TODO
+    :returns: TODO
+
+    """
+    rm_cnt, keep_cnt = 0, 0
+    for f, m in marks:
+        fout.write('{0}/bin/rm -f {1}\n'.format('#' if m else '', f))
+        rm_cnt += not m
+        keep_cnt += m
+    fout.write('echo {0} removed and {1} kept.\n'.format(rm_cnt, keep_cnt))
 
 
 @click.command()
-def main(args=None):
-    """Console script for bakman"""
-    click.echo("Replace this message by putting your code into "
-               "bakman.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('-j', '--use-json', is_flag=True,
+              help='Use json as input.')
+@click.option('-f', '--rulefile', default='BakmanFile',
+              help='The rulefile in JSON or YAML.')
+@click.option('-r', '--regex', default=None,
+              help='The regex for label the date of the file in the filename.')
+@click.option('-g', '--group-regex', default=None,
+              help='The regex for indicating files belongs to the same backup snapshot.')
+@click.option('-o', '--output', default=None,
+              help='Output the remove command into a bash script for inspecting.')
+@click.argument('files', nargs=-1)
+def console(use_json, rulefile, regex, output, group_regex, files):
+    """ Parse the rules expressed in a dict object
+
+    :dct: TODO
+    :returns: TODO
+
+    """
+    rules = load_rules(rulefile, use_json)
+    marker = Marker.from_rule(rules)
+    if regex is not None:
+        labeller = RegexLabeller(files, regex)
+    else:
+        labeller = StatLabeller(files)
+
+    if group_regex:
+        labeller = BinderLabeller(files, group_regex, labeller.__class__)
+
+    if output is not None:
+        with open(output, 'w') as fout:
+            print_actions(fout, labeller.mark_with(marker))
+    else:
+        print_actions(sys.stdout, labeller.mark_with(marker))
 
 
 if __name__ == "__main__":
-    main()
+    console()
